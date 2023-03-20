c801309f368d31fef605fe78180d9dbd773e1e64
Barbara Vreede <b.vreede@esciencecenter.nl>
2023-03-20 12:45:58 +0100

    refer to NLeSC in banner

--- components/Banner.vue
+++ components/Banner.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex gap-10 m-2 items-center">
-    <img src="~/static/nlesc_logo.png" alt="EUCP Logo">
+    <img src="~/static/nlesc_logo.png" alt="Netherlands eScience Center Logo">
     <h1 class="text-2xl">
-      Overview of EUCP Storyboards
+      Essentials for Research Software Support
     </h1>
   </div>
 </template>
