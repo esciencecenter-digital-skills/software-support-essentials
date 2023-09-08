f221c0f1e983b226b6bba16f5ad21f29d570a543
Barbara Vreede <b.vreede@esciencecenter.nl>
2023-09-08 10:59:51 +0200

    expand search box

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex flex-col w-screen bg-gentleBlue h-screen overflow-auto">
     <Banner />
-    <input v-model="query" type="search" class="w-1/3 m-4 p-2 self-end" placeholder="search">
-    <div class="flex flex-col p-6 gap-2">
+    <input v-model="query" type="search" class="w-2/3 mt-4 mr-4 p-2 self-end" placeholder="search">
+    <div class="flex flex-col pt-4 pb-6 pl-6">
       <!-- categories -->
       <div v-for="category in categories" :key="category" class="flex flex-wrap gap-4 mb-8">
         <h2 class="prose-2xl font-display font-bold text-eSciencePurple w-full pl-2">
