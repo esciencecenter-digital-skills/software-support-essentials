ea6b1c31b61105d1fc68e04c18009c8f0a8ebf53
Barbara Vreede <b.vreede@esciencecenter.nl>
2023-09-08 11:00:03 +0200

    commit footer

---
+++ components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div class="decoration-slice bg-gentleBlue p-4">
+  </div>
+    <div id="footer" class="bg-eSciencePurple p-2">
+      <p class="font-body text-eScienceWhite text-sm text-center">
+        These materials are developed by the <a href="https://www.esciencecenter.nl">Netherlands eScience Center</a>. Licensed under <a href="https://creativecommons.org/licenses/by/4.0/">CC-BY 4.0</a> unless otherwise noted.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
